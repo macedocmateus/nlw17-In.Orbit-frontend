c2fd64bbc6e90f52e25c733129b1c2f521a2eb99
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-11 21:26:05 -0300

    add empty page

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,20 @@
+import {Plus} from "lucide-react"
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+
 export function App() {
-  return <h1 className="text-red-500">Hello World!</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="Logo da orbit" />
+      <img src={letsStart} alt="imagem de ilustração da orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
+      </p>
+      <button type="button" className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600 cursor-pointer">
+        <Plus className="size-4" />
+        Cadastrar meta
+        </button>
+    </div>
+  )
 }
